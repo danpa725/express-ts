95afe805289449993a671fa7156605cbfef74758
June <danpa725@cau.ac.kr>
2021-08-24 16:37:28 +0900

    Make: cat-info Create, Read routers.

--- dist/app.js
+++ dist/app.js
@@ -1,27 +1,66 @@
 "use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from) {
+    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
+        to[j] = from[i];
+    return to;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app_model_1 = require("./app.model");
 var app = express();
+app.use(express.json());
 app.use(function (req, res, next) {
     console.log(req.rawHeaders[0]);
     next();
 });
-app.get("/", function (req, res) {
-    res.send({
-        message: "server init at 8000 port",
-    });
-});
-app.get("/cats", function (req, res, next) {
-    console.log({
-        cats: "CatInfo를 출력합니다.",
-    });
-    next();
-});
 app.get("/cats", function (req, res) {
-    res.send({
-        cats: app_model_1.CatInfos,
-    });
+    try {
+        var cats = app_model_1.CatInfos;
+        res.status(200).send({
+            success: true,
+            data: { cats: cats },
+        });
+    }
+    catch (error) {
+        res.status(400).send({
+            success: false,
+            message: "고양이 데이터 조회 실패",
+        });
+    }
+});
+app.get("/cats/:id", function (req, res) {
+    var catId = req.params.id;
+    try {
+        var specificCat = app_model_1.CatInfos.find(function (cat) { return cat.id === catId; });
+        res.status(200).send({
+            success: true,
+            data: {
+                cat: specificCat,
+            },
+        });
+    }
+    catch (error) {
+        res.status(400).send({
+            success: false,
+            message: error,
+        });
+    }
+});
+app.post("/cats", function (req, res) {
+    try {
+        var catData = req.body;
+        var updatedData = __spreadArray(__spreadArray([], app_model_1.CatInfos), [catData]);
+        res.status(200).send({
+            success: true,
+            data: { updatedData: updatedData },
+        });
+    }
+    catch (error) {
+        res.status(400).send({
+            success: false,
+            message: "고양이 추가 실패",
+        });
+    }
 });
 app.use(function (req, res, next) {
     res.send({
--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,85 @@
+import { error } from "console";
 import * as express from "express";
 import { CatInfos } from "./app.model";
+import { CatInfo } from "./types/types";
 
 const app: express.Express = express();
+//* body-parser: json 미들웨어
+app.use(express.json());
 
-//미들웨어: 가장 처음으로 처리하는 라우터!
-//! 라우터는 순차적으로 실행 -> 실행 순서 및 위치가 중요.
+//* logging middleware
 app.use((req, res, next) => {
     // 로깅 미들웨어
     console.log(req.rawHeaders[0]);
     next();
 });
 
-app.get("/", (req: express.Request, res: express.Response) => {
-    res.send({
-        message: "server init at 8000 port",
-    });
-});
-
-app.get("/cats", (req, res, next) => {
-    console.log({
-        cats: "CatInfo를 출력합니다.",
-    });
-    next();
-});
-
+//* Read: 고양이 전체 데이터
 app.get("/cats", (req, res) => {
-    res.send({
-        cats: CatInfos,
-    });
+    try {
+        //* DB 서버에서 cats 데이터 조회 가정
+        const cats = CatInfos;
+
+        //* 상태 status()로 200 성공 주기
+        res.status(200).send({
+            success: true,
+            data: { cats },
+        });
+    } catch (error) {
+        //* 상태 status()로 400 실패 주기
+        res.status(400).send({
+            success: false,
+            message: "고양이 데이터 조회 실패",
+        });
+    }
 });
 
-// 에러 처리 라우터
+//*Read: 고양이 ID로 조회하기
+//! :id param전달
+app.get("/cats/:id", (req, res) => {
+    //* params에서 id 추출
+    const catId = req.params.id;
+
+    try {
+        const specificCat = CatInfos.find((cat) => cat.id === catId);
+
+        res.status(200).send({
+            success: true,
+            data: {
+                cat: specificCat,
+            },
+        });
+    } catch (error) {
+        res.status(400).send({
+            success: false,
+            message: error,
+        });
+    }
+});
+
+//*Create: 새로운 고양이 추가
+app.post("/cats", (req, res) => {
+    try {
+        //* body로 전달된 데이터 조회
+        const catData: CatInfo = req.body;
+
+        const updatedData = [...CatInfos, catData];
+
+        //* 상태 status()로 200 성공 주기
+        res.status(200).send({
+            success: true,
+            data: { updatedData },
+        });
+    } catch (error) {
+        //* 상태 status()로 400 실패 주기
+        res.status(400).send({
+            success: false,
+            message: "고양이 추가 실패",
+        });
+    }
+});
+
+//* error handling middleware
 app.use((req, res, next) => {
     res.send({
         error: 404,
